582b6ff5d8888d7ee05f796015e1567727cfffa9
vaishnav kedar <vaishnavk9420@gmail.com>
2026-05-10 12:20:42 +0530

    changes

--- server.js
+++ server.js
@@ -3,47 +3,19 @@ const fs = require("fs");
 const path = require("path");
 
 const PORT = process.env.PORT || 3000;
-const PUBLIC_DIR = path.join(__dirname, "public");
+const INDEX_PATH = path.join(__dirname, "public", "index.html");
 
-const MIME_TYPES = {
-  ".html": "text/html; charset=utf-8",
-  ".css": "text/css; charset=utf-8",
-  ".js": "text/javascript; charset=utf-8",
-  ".png": "image/png",
-  ".jpg": "image/jpeg",
-  ".jpeg": "image/jpeg",
-  ".svg": "image/svg+xml",
-  ".ico": "image/x-icon"
-};
-
-function sendFile(res, filePath) {
-  const ext = path.extname(filePath).toLowerCase();
-  const contentType = MIME_TYPES[ext] || "application/octet-stream";
-
-  fs.readFile(filePath, (err, data) => {
+const server = http.createServer((req, res) => {
+  fs.readFile(INDEX_PATH, (err, data) => {
     if (err) {
-      res.writeHead(404, { "Content-Type": "text/plain; charset=utf-8" });
-      res.end("404 - Not Found");
+      res.writeHead(500, { "Content-Type": "text/plain; charset=utf-8" });
+      res.end("500 - Server Error");
       return;
     }
 
-    res.writeHead(200, { "Content-Type": contentType });
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
     res.end(data);
   });
-}
-
-const server = http.createServer((req, res) => {
-  const safePath = path.normalize(req.url).replace(/^\/+/, "");
-  const requestedPath = safePath === "" ? "index.html" : safePath;
-  const filePath = path.join(PUBLIC_DIR, requestedPath);
-
-  if (!filePath.startsWith(PUBLIC_DIR)) {
-    res.writeHead(400, { "Content-Type": "text/plain; charset=utf-8" });
-    res.end("400 - Bad Request");
-    return;
-  }
-
-  sendFile(res, filePath);
 });
 
 server.listen(PORT, () => {
